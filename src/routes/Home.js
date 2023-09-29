232bbb30ee061f61aa86211c6f6cef6a16b23b67
jezraelhope <jezraelhope@gmail.com>
2023-09-29 15:56:25 -0500

    update home styling

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
 
 import "../styles/home.css";
+import dragon from "../assets/dragon.jpg";
 
 const Home = () => {
   return (
-    <div className="bg-dragon bg-cover h-full p-1 flex flex-col">
-      <h1 className="welcome bg-trgray text-asred font-bold py-16 px-auto mt-[10rem] mb-8 mx-auto text-center rounded-[4rem] w-4/5 max-w-6xl">
-        {" "}
-        Welcome to Adventshare!{" "}
-      </h1>
-      <p className="home-text w-4/5 max-w-6xl text-center mx-auto my-10">
-        {" "}
-        This app was built for TTRPG players and Game Masters to share their
-        adventures. Whether it was just a one shot, or a multi-year journey,
-        Adventshare is the place to write your final tale...{" "}
-      </p>
-      <Link
-        to="/stories"
-        className="bg-asred rounded-full p-3 mx-auto my-20 text-center"
-      >
-        <button className="allstories-button p-1">View All Stories</button>
-      </Link>
+    <div className="flex flex-row">
+      <img src={dragon} alt="" className="w-1/2" />
+      <div className="w-3/5  py-16 px-auto mt-12 mb-8 mx-auto text-center rounded-[4rem]  max-w-4xl">
+        <h1 className="welcome text-asred font-bold px-4 mt-4">
+          {" "}
+          Welcome to Adventshare!{" "}
+        </h1>
+        <p className="home-text w-4/5 max-w-6xl text-center mx-auto my-10 font-CinzelDeco">
+          {" "}
+          This app was built for TTRPG players and Game Masters to share their
+          adventures. Whether it was just a one shot, or a multi-year journey,
+          Adventshare is the place to write your final tale...{" "}
+        </p>
+        <Link
+          to="/stories"
+          className="bg-asred rounded-full p-3 mx-auto my-20 text-center"
+        >
+          <button className="allstories-button p-1">View All Stories</button>
+        </Link>
+      </div>
     </div>
   );
 };
