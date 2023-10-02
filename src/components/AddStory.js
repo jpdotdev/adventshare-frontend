c5ee225f5206bc784e677452fd2b6d5dc7287106
jezraelhope <jezraelhope@gmail.com>
2023-10-02 16:50:41 -0400

    add create story styling

--- src/components/AddStory.js
+++ src/components/AddStory.js
@@ -3,6 +3,8 @@ import Adventshare from "../APIs/Adventshare";
 import { StoriesContext } from "../context/StoriesContext";
 import useLocalState from "../hooks/useLocalStorage";
 
+import "../styles/createstory.css";
+
 const AddStory = () => {
   const { addStories } = useContext(StoriesContext);
   const [character, setCharacter] = useState("");
@@ -39,41 +41,53 @@ const AddStory = () => {
   };
 
   return (
-    <div>
-      <form>
-        <div>
-          <label>Character Name</label>
-          <input
-            value={character}
-            onChange={(e) => setCharacter(e.target.value)}
-            type="text"
-            placeholder="Character Name"
-          />
-        </div>
-        <div>
-          <label>Party Name</label>
-          <input
-            value={party}
-            onChange={(e) => setParty(e.target.value)}
-            type="text"
-            placeholder="Party Name"
-          />
-        </div>
-        <div>
-          <label>Story Entry</label>
-          <textarea
-            value={story}
-            onChange={(e) => setStory(e.target.value)}
-            type="text"
-            placeholder="Story Entry"
-          />
-        </div>
-      </form>
-
-      <button type="submit" onClick={handleSubmit}>
-        {" "}
-        Add Story{" "}
-      </button>
+    <div className="mt-32">
+      <h1 className="header-create-story mb-10 text-center font-CinzelDeco font-bold mx-auto max-w-xl min-w-2/5">
+        Create a Story
+      </h1>
+      <div className="create-story min-w-[50%] max-w-xl border border-white mx-auto py-8 px-10 font-CinzelDeco">
+        <form className=" mx-auto">
+          <div className="flex flex-row justify-between min-w-[60%] max-w-lg m-3 items-center">
+            <label for="character-name">Character Name:</label>
+            <input
+              id="character-name"
+              value={character}
+              onChange={(e) => setCharacter(e.target.value)}
+              type="text"
+              className="text-dkpurple bg-beige shadow-inner shadow-dkpurple"
+            />
+          </div>
+          <div className="flex flex-row justify-between min-w-[60%] max-w-lg m-3 items-center">
+            <label for="party-name">Party Name:</label>
+            <input
+              id="party-name"
+              value={party}
+              onChange={(e) => setParty(e.target.value)}
+              type="text"
+              className="text-dkpurple bg-beige shadow-inner shadow-dkpurple"
+            />
+          </div>
+          <div className="flex flex-col justify-between m-3 items-start">
+            <label for="story-area">Story Entry:</label>
+            <textarea
+              id="story-area"
+              value={story}
+              onChange={(e) => setStory(e.target.value)}
+              type="text"
+              placeholder="Enter your story here"
+              className="w-full h-56 my-3 p-3 text-dkpurple bg-beige shadow-inner shadow-dkpurple"
+            />
+          </div>
+        </form>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="bg-asred rounded-full py-2 px-4"
+        >
+          {" "}
+          Add Story{" "}
+        </button>
+      </div>
     </div>
   );
 };
